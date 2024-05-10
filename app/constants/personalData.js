8d049cde34f33b27bddf7cc0d3a5aa921f130295
htunyarzar80 <htunyarzar2001@gmail.com>
2024-05-10 17:05:11 +0630

    [Fixing] personalData

--- app/constants/personalData.js
+++ app/constants/personalData.js
@@ -1,13 +1,4 @@
-import {
-  FaBug,
-  FaCss3,
-  FaFigma,
-  FaHtml5,
-  FaJava,
-  FaJs,
-  FaNode,
-  FaReact,
-} from "react-icons/fa";
+import { FaBug, FaCss3, FaFigma, FaHtml5, FaJava, FaJs, FaNode, FaReact } from "react-icons/fa";
 import {
   SiAdobephotoshop,
   SiAdobexd,
@@ -31,48 +22,31 @@ export const personalData = [
       {
         title: "Web Development",
         icons: [
-          { icon: <FaHtml5 style={{ fontSize: "35px", color: "#E44D26" }} /> },
-          { icon: <FaCss3 style={{ fontSize: "35px", color: "#264de4" }} /> },
-          { icon: <FaReact style={{ fontSize: "35px", color: "#61DAFB" }} /> },
-          { icon: <FaJs style={{ fontSize: "35px", color: "#F0DB4F" }} /> },
-          {
-            icon: (
-              <SiNextdotjs style={{ fontSize: "35px", color: "#2d2d2d" }} />
-            ),
-          },
-          { icon: <SiFramer style={{ fontSize: "35px", color: "#0055FF" }} /> },
+          { icon: <FaHtml5 style={{ fontSize: "35px", color: "#E44D26" }} />}, 
+          { icon: <FaCss3 style={{ fontSize: "35px", color: "#264de4" }} />}, 
+          { icon: <FaReact style={{ fontSize: "35px", color: "#61DAFB" }} />}, 
+          { icon: <FaJs style={{ fontSize: "35px", color: "#F0DB4F" }} />}, 
+          { icon: <SiNextdotjs style={{ fontSize: "35px", color: "#2d2d2d" }} />},
+          { icon: <SiFramer style={{ fontSize: "35px", color: "#0055FF" }} />}, 
+          
+
         ],
       },
       {
         title: "Backend Development",
         icons: [
-          { icon: <FaFigma style={{ fontSize: "35px", color: "#F24E1E" }} /> },
-          {
-            icon: <SiAdobexd style={{ fontSize: "35px", color: "#FF61F6" }} />,
-          },
-          {
-            icon: (
-              <SiAdobephotoshop
-                style={{ fontSize: "35px", color: "#31A8FF" }}
-              />
-            ),
-          },
+          { icon: <FaJava style={{ fontSize: "35px", color: "#007396" }} />},
+          { icon: <SiDotnet style={{ fontSize: "35px", color: "#512BD4" }} />}, 
+          { icon: <SiSpringboot style={{ fontSize: "35px", color: "#6DB33F" }} /> },
+          { icon: <SiNodedotjs style={{ fontSize: "35px", color: "#68A063" }} />}, 
         ],
       },
       {
         title: "UI/UX Design",
         icons: [
-          { icon: <FaFigma style={{ fontSize: "35px", color: "#F24E1E" }} /> },
-          {
-            icon: <SiAdobexd style={{ fontSize: "35px", color: "#FF61F6" }} />,
-          },
-          {
-            icon: (
-              <SiAdobephotoshop
-                style={{ fontSize: "35px", color: "#31A8FF" }}
-              />
-            ),
-          },
+          { icon: <FaFigma style={{ fontSize: "35px", color: "#F24E1E" }} />}, 
+          { icon: <SiAdobexd style={{ fontSize: "35px", color: "#FF61F6" }} />}, 
+          { icon: <SiAdobephotoshop style={{ fontSize: "35px", color: "#31A8FF" }} />}, 
         ],
       },
     ],
