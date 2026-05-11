50fc750135ad77937824a49f539ce1d5f9ae7e18
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-11 16:05:35 +0900

    fixed-_app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, Suspense } from "react";
 import { ThemeProvider } from "next-themes";
+import { I18nextProvider } from "react-i18next";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
-// import i18n from "@/i18n";
+import i18n from "@/i18n";
 import dynamic from "next/dynamic";
 import RootLayout from "@/app/layout";
 import Logo from "../public/logo_icon.png";
@@ -39,6 +40,7 @@ const MyApp = ({ Component, pageProps }) => {
 
   return (
     <ThemeProvider>
+      <I18nextProvider i18n={i18n}>
         <RootLayout>
           <AnimatePresence mode="wait">
             <motion.div key={router.route} className="h-full">
@@ -48,6 +50,7 @@ const MyApp = ({ Component, pageProps }) => {
             </motion.div>
           </AnimatePresence>
         </RootLayout>
+      </I18nextProvider>
     </ThemeProvider>
   );
 };
